b624ee1007bdf8c45dd0c7ae8f9e095f7c91b6db
Vitor Parente <vitorebparente@hotmail.com>
2024-10-28 18:01:58 -0300

    Tela de beneficiados

--- front_end_integrador/lib/beneficiados.dart
+++ front_end_integrador/lib/beneficiados.dart
@@ -1,10 +1,184 @@
 import 'package:flutter/material.dart';
 
-class BeneficiadosPage extends StatelessWidget {
-  const BeneficiadosPage({super.key});
+class BeneficiadosPage extends StatefulWidget {
+  @override
+  _BeneficiadosPageState createState() => _BeneficiadosPageState();
+}
+
+class _BeneficiadosPageState extends State<BeneficiadosPage> {
+  int _selectedIndex = 2;
+
+  void _onItemTapped(int index) {
+    if (index != _selectedIndex) {
+      setState(() {
+        _selectedIndex = index;
+      });
+
+      if (index == 0) {
+        Navigator.pushReplacementNamed(context, '/home'); // Página inicial
+      } else if (index == 1) {
+        Navigator.pushReplacementNamed(
+            context, '/home'); // Página de empréstimo
+      } else if (index == 2) {
+        Navigator.pushReplacementNamed(
+            context, '/beneficiados'); // Página de beneficiados
+      } else if (index == 3) {
+        Navigator.pushReplacementNamed(context, '/itens'); // Página de itens
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF0000FF),
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Padding(
+          padding: const EdgeInsets.only(top: 5.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Página de Beneficiados',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              SizedBox(height: 4),
+            ],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0, top: 10.0),
+            child: Icon(Icons.notifications, color: Colors.white, size: 28),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0, top: 10.0),
+            child: Icon(Icons.account_circle, color: Colors.white, size: 28),
+          ),
+        ],
+        toolbarHeight: 80, // Altura ajustada para 80px
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search and filter',
+                  suffixIcon: Icon(Icons.filter_list),
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5, // Substitua pelo tamanho da lista de beneficiados
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    margin: EdgeInsets.symmetric(vertical: 8),
+                    child: ListTile(
+                      title: Text(
+                        'Alberto',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text('Aluno'),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.edit, color: Colors.grey),
+                            onPressed: () {
+                              // Lógica de edição
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete, color: Colors.grey),
+                            onPressed: () {
+                              // Lógica de exclusão
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 10),
+            FloatingActionButton(
+              onPressed: () {
+                // Lógica para adicionar um novo beneficiado
+              },
+              backgroundColor: Color.fromARGB(100, 86, 100, 245),
+              child: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2), // Cor da sombra
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/images/icon_home.png')),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/images/icon_import.png')),
+              label: 'Importar',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people),
+              label: 'Beneficiados',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Itens',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Color(0xFF1A00FF),
+          unselectedItemColor: Color.fromRGBO(64, 64, 64, 100),
+          backgroundColor: Colors.white,
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
   }
 }
